b294d92285e87a16eda4885f134c5edd1e85a87b
HottaShomu <hottasho@cy-com.co.jp>
2025-06-04 15:49:25 +0900

    削除、編集追加

--- src/main/java/com/example/todo_management/controller/TodoController.java
+++ src/main/java/com/example/todo_management/controller/TodoController.java
@@ -5,6 +5,7 @@ import com.example.todo_management.service.TodoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.todo_management.form.TodoForm;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,11 +36,32 @@ public class TodoController {
     }
 
     @PostMapping("/new")
-    public String createTodo(TodoForm todoForm, Model model){
+    public String createTodo(TodoForm todoForm){
         todoService.createTodo(todoForm);
 
-        List<Todo> todos = todoService.getAllTodos();
-        model.addAttribute("todos", todos);
-        return "todo/todo-list";
+        //List<Todo> todos = todoService.getAllTodos();
+        //model.addAttribute("todos", todos);
+        //return "todo/todo-list";
+        return "redirect:/todos";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteTodo(@PathVariable long id){
+        todoService.deleteTodo(id);
+        return "redirect:/todos";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editTodo(@PathVariable long id, Model model){
+        Todo todo = todoService.getTodoById(id);
+        model.addAttribute("todo", todo);
+
+        return "todo/todo-edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String updateTodo(@PathVariable long id, Todo todo){
+        todoService.updateTodo(id,todo);
+        return "redirect:/todos";
     }
 }

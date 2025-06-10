b71838b6875340414b6a9d8e4faae8a5aa06dda4
HottaShomu <hottasho@cy-com.co.jp>
2025-06-10 16:22:45 +0900

    user堀田

--- src/main/java/com/example/todo_management/entity/User.java
+++ src/main/java/com/example/todo_management/entity/User.java
@@ -6,10 +6,10 @@ import java.time.LocalDateTime;
 
 @Data
 public class User {
-    public class users {
+    
     private long userId;
     private String username;
     private String password;
     private LocalDateTime createdAt;
-    }
+    
 }
